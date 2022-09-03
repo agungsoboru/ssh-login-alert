aa50eaf8dc1747bf926920ab2a31ea5739132bd2
gung <71138589+agungsoboru@users.noreply.github.com>
2022-09-04 01:34:08 +0800

    Create login_notify.sh

---
+++ login_notify.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Content of /etc/ssh/login_notify.sh
+TELEGRAM_TOKEN="987654321:ABCDEFGHIJKLMNopqrstUVWXYZ123456789"
+CHAT_ID=""
+
+if [ ${PAM_TYPE} = "open_session" ]; then
+  MESSAGE="$PAM_USER@$PAM_RHOST: knock knock via $PAM_SERVICE"
+  curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" -d chat_id="$CHAT_ID" -d text="$MESSAGE" > /dev/null 2>&1
+fi
